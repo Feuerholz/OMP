d1f14c98a07cf6071f48d6afa4af5421fc97edf3
Sean Westphal <Sean.Westphal97@gmail.com>
2018-11-03 00:01:05 +0100

    initial commit

---
+++ OMP/Model/Mod.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMP.Model
+{
+    public enum Mod { NM=0, NF=1, EZ=2, HD=8, HR=16, SD=32, DT=64, EZNF=3, HDNF=9, HRNF=17, DTNF=65, EZHD=10, EZDT=66, HDHR=24, HDDT=72 }
+}
